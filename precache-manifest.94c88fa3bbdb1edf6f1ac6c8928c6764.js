712627e33cce5f5bdfd601b8b1bf0ab799bcb476
2kartheek10 <kxs180091@utdallas.edu>
2020-08-15 23:38:40 -0500

    Updates

---
+++ precache-manifest.94c88fa3bbdb1edf6f1ac6c8928c6764.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27be619545cb8bd45e63f03fe72c3238",
+    "url": "/Restaurant_React/index.html"
+  },
+  {
+    "revision": "1a54783d5048926c6912",
+    "url": "/Restaurant_React/static/css/2.dc0efecb.chunk.css"
+  },
+  {
+    "revision": "3c899543b4e4b67b2e70",
+    "url": "/Restaurant_React/static/css/main.113766f8.chunk.css"
+  },
+  {
+    "revision": "1a54783d5048926c6912",
+    "url": "/Restaurant_React/static/js/2.9be11856.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Restaurant_React/static/js/2.9be11856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c899543b4e4b67b2e70",
+    "url": "/Restaurant_React/static/js/main.b2be73d0.chunk.js"
+  },
+  {
+    "revision": "1291cb5a0277762c04f7",
+    "url": "/Restaurant_React/static/js/runtime-main.2785bfca.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restaurant_React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restaurant_React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restaurant_React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restaurant_React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restaurant_React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Restaurant_React/static/media/logo.5d5d9eef.svg"
+  }
+]);
